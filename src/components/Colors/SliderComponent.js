b60ba832fe245bda4db14f90f96fd8719e4255c3
dbryan17 <dfbryan17@gmail.com>
2023-04-09 22:11:50 -0400

    commented color stuff

--- src/components/Colors/SliderComponent.js
+++ src/components/Colors/SliderComponent.js
@@ -4,18 +4,22 @@ import ColorDisplayer from "./ColorDisplayComponent";
 import "./colors.css";
 import { useColorsStore } from "../../store/zustandTest";
 
+/*
+props: num, the number the user picked from the input box for how many colors
+returns: the slider (hue picker), and the color displayer which is all the colors displayed
+descripion: this is the color picker, it uses a hue picker slider from a npm package 
+and uses the color picked from that to generate the color boxes which is a child compoenent
+*/
 function ColorPicker({ num }) {
+  // is the color where the slider currently is and allows it to move, starts as a tealish blue
   const [selectedColor, setSelectedColor] = useState({
     hex: "#00c4ff",
     hsl: { h: 193.8795180722891, s: 1, l: 0.5, a: 1 },
   });
-  const [colors, setColors] = useState();
+  // references a random div that the component is wrapped in, that gets passed down to determine the
+  // height of the color boxes
+  // TODO could probably do without this as the height of the slider doesn't change
   const wrapperRef = useRef(null);
-  const set = useColorsStore((state) => state.set);
-
-  // setWidth(wrapperRef.current.clientWidth);
-  const [width, setWidth] = useState(null);
-
   return (
     <div id="slider-cont">
       <div ref={wrapperRef}>
@@ -31,8 +35,7 @@ function ColorPicker({ num }) {
         id="colors"
         selectedColor={selectedColor}
         num={num}
-        width={wrapperRef}
-        height={wrapperRef}
+        wrapper={wrapperRef}
       />
     </div>
   );
