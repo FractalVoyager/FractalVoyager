67c1f16640391ee2cd5e9501309558e9159373c4
Dakota Bryan <Dakota.Bryan@ibm.com>
2023-04-09 22:11:50 -0400

    commented color stuff

--- src/components/Colors/ColorDisplayComponent.js
+++ src/components/Colors/ColorDisplayComponent.js
@@ -3,41 +3,50 @@ import { useState, useEffect } from "react";
 import chroma from "chroma-js";
 import { useColorsStore } from "../../store/zustandTest";
 
-function ColorDisplayer({ selectedColor, num, width, height }) {
+/*
+props: selectedColor: color that slider selected, num: how many colors, wrapper: div to help style
+returns: gradient of selected colors with the number of colors being num
+description: calculates a color gradient of selected color and displays it 
+ */
+function ColorDisplayer({ selectedColor, num, wrapper }) {
+  // local state of all the colors
   const [colors, setColors] = useState();
+  // sets the global state varaible which is the colors and numColors
+  // setting num colors here is it is done at the same time - prboably don't need to
   const set = useColorsStore((state) => state.set);
 
+  // on change of selectedColor or num, calculate color graident and set
   useEffect(() => {
     let cs = [];
+    // because black is always a color
     let amt = num - 1;
     cs.push([0, 0, 0]);
     for (let i = 0; i < amt; i++) {
+      // changing the lightness value of hsl color
       let l = 0.1 + (0.8 / (amt - 1)) * i;
       let newColor = { h: selectedColor.hsl.h, s: 1, l: l };
-
+      // converts hsl to rgb
       let rgb = chroma(newColor).rgb();
       cs.push(rgb);
     }
-
     setColors(cs);
     set(cs, num);
-    console.log(cs, selectedColor);
   }, [selectedColor, num]);
 
+  // calculate width of each box, and height of each box
   let wStr;
   let hStr;
-  if (width && colors) {
+  // only if wrapper and colors are defned
+  if (wrapper && colors) {
     wStr =
-      Math.floor(width.current.clientWidth / colors.length).toString() + "px";
-    hStr = Math.floor(width.current.clientHeight * 3).toString() + "px";
+      Math.floor(wrapper.current.clientWidth / colors.length).toString() + "px";
+    hStr = Math.floor(wrapper.current.clientHeight * 3).toString() + "px";
   }
-  // if (height && colors) {
-  // }
-
-  //// // // console.log(colors);
-  if (colors && width) {
+  // also if we have colors and wrapper, we're good to return component, otherwise, return nothing
+  if (colors && wrapper) {
     return (
       <div id="colors-cont">
+        {/* map the colors to div elements with key of they're color */}
         {colors.map((color) => {
           let str = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
           return (
